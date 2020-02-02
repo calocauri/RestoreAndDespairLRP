fb566b75860fd593a1ad4900fa4e42771f4ebf04
AndrePeres <andre.peres@tagwizz.com.mx>
2020-02-01 23:25:21 -0600

    UI stuff

---
+++ Assets/UI/EndView.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EndView : MonoBehaviour {
+    public PlayerController playerOne, playerTwo;
+    public Transform cameraTransform, P1Transform, P2Transform;
+    void OnEnable() {
+        Camera.main.transform.SetPositionAndRotation(cameraTransform.position, cameraTransform.rotation);
+        playerOne.transform.SetPositionAndRotation(P1Transform.position, P1Transform.rotation);
+        playerTwo.transform.SetPositionAndRotation(P2Transform.position, P2Transform.rotation);
+    }
+}

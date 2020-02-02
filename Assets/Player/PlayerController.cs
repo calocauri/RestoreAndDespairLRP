fb3f7b6afb5195d3e69ac23e9a94de2a6c4bb7b9
AndrePeres <andre.peres@tagwizz.com.mx>
2020-02-02 16:48:16 -0600

    no character swap

--- Assets/Player/PlayerController.cs
+++ Assets/Player/PlayerController.cs
@@ -19,6 +19,9 @@ public class PlayerController : MonoBehaviour {
     [SerializeField]
     private AudioClip screamClip;
 
+    [SerializeField]
+    private Transform weaponSocked;
+
     private new Rigidbody rigidbody;
     private AudioSource audioSource;
 
